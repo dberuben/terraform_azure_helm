20776c8971a0e5716451e054c1c198af6dc77501
Daniel BERUBEN <daniel.beruben@thalesdigital.io>
2019-12-14 16:45:29 -0500

    update provider

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
-provider "azurerm" {}
+provider "azurerm" {
+  subscription_id = var.subscription_id
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.tenant_id
+
+}
 
 provider "kubernetes" {
   host                   = azurerm_kubernetes_cluster.k8s.kube_config.0.host
