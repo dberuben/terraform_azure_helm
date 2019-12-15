520065bfd619f13ce824a54076e074a212bd2b9a
Daniel BERUBEN <daniel.beruben@thalesdigital.io>
2019-12-15 10:34:56 -0500

    new

--- cert-manager.tf
+++ cert-manager.tf
@@ -17,8 +17,13 @@ resource "kubernetes_namespace" "cert-manager" {
 
 resource "null_resource" "cert-manager-crds" {
   depends_on = [kubernetes_namespace.cert-manager]
+
+   provisioner "local-exec" {
+   command = "apk add --update --no-cache bash python3 libffi openssl curl ln -s /usr/bin/python3 /usr/bin/python && curl -L https://aka.ms/InstallAzureCli | bash &&  && curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x ./kubectl && mv ./kubectl /usr/local/bin/kubectl"
+
+}
   provisioner "local-exec" {
-    command = "apk add curl && curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x ./kubectl && mv ./kubectl /usr/local/bin/kubectl && export KUBECONFIG=admin-${terraform.workspace} && kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.10/deploy/manifests/00-crds.yaml && sleep 60"
+    command = "export KUBECONFIG=admin-${terraform.workspace} && kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.10/deploy/manifests/00-crds.yaml && sleep 60"
   }
 
 }
